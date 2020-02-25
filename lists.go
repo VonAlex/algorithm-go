74f9feb9209f277e290e4da2f26bb6f98634dc1e
VonAlex <Ahappyhappen@163.com>
2020-02-26 00:03:37 +0800

    :sparkles: solve reverse print list

--- lists.go
+++ lists.go
@@ -1,5 +1,10 @@
 package leetcode
 
+import (
+	"container/list"
+	"fmt"
+)
+
 // ListNode definition for singly-linked list.
 type ListNode struct {
 	Val  int
@@ -7,7 +12,7 @@ type ListNode struct {
 }
 
 /**
- * 题2 两数相加
+ * LeetCode 题2 两数相加
  *
  * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
  * 输出：7 -> 0 -> 8
@@ -80,4 +85,141 @@ func AddTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 	}
 	return dummy.Next
+}
+
+/**
+ * 剑指 offer 面试题06 从尾到头打印链表
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ *
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ */
+
+// 解法1 使用栈结构，这是最容易想到的
+// 时间复杂度和空间复杂度都是 O(N)
+func ReversePrint(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	stack := list.New()
+	walk := head
+	for walk != nil {
+		stack.PushFront(walk.Val) // 使用栈结构
+		walk = walk.Next
+	}
+	revers := []int{}
+	for e := stack.Front(); e != nil; e = e.Next() {
+		revers = append(revers, e.Value.(int))
+	}
+	return revers
+}
+
+// 解法2 两次循环，先获得 list 的节点个数，然后倒着放置节点
+// 时间复杂度 O(N)，空间复杂度 O(1)
+func ReversePrint2(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	nodeCnt := 0
+	curr := head
+	// 第一次遍历 list，统计链表中有多少个节点
+	for curr != nil {
+		nodeCnt++
+		curr = curr.Next
+	}
+	revers := make([]int, nodeCnt)
+	curr = head
+	// 再次遍历 list，从后往前放 val
+	for i := nodeCnt - 1; i >= 0; i-- {
+		revers[i] = curr.Val
+		curr = curr.Next
+	}
+	return revers
+}
+
+// 解法3 翻转数组
+func ReversePrint3(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	revers := []int{}
+	curr := head
+	// 遍历 list，将 val 顺序放入结果
+	for curr != nil {
+		revers = append(revers, curr.Val)
+		curr = curr.Next
+	}
+	nodeCnt := len(revers)
+	// 将结果数组进行翻转
+	for i, j := 0, nodeCnt-1; i < j; {
+		revers[i], revers[j] = revers[j], revers[i]
+		i++
+		j--
+	}
+	return revers
+}
+
+// 解法4 翻转链表
+// 这个方法有个弊端，就是会修改原始 list，这很有可能不合题意
+func ReversePrint4(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	// 翻转链表
+	var prev, next *ListNode
+	curr := head
+	for curr != nil {
+		next = curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	revers := []int{}
+	curr = prev
+	for curr != nil {
+		revers = append(revers, curr.Val)
+		curr = curr.Next
+	}
+	// 可以再把 list 翻转过来
+	return revers
+}
+
+// 解法5 递归法
+// 当链表过长时，可能出现函数调用栈溢出的异常
+func ReversePrint5(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	revers := []int{}
+	curr := head
+	nextVal := reversePrintHelper(curr, &revers)
+	revers = append(revers, nextVal)
+	return revers
+}
+
+func reversePrintHelper(node *ListNode, res *[]int) int {
+	if node.Next == nil { // 是否到了最后一个节点
+		return node.Val
+	}
+	nextVal := reversePrintHelper(node.Next, res)
+	*res = append(*res, nextVal)
+	return node.Val
+}
+
+/***************************** 辅助函数 *********************************/
+
+// ListPrint 正向打印 list
+func ListPrint(node *ListNode) {
+	if node == nil {
+		return
+	}
+	for node != nil {
+		fmt.Print(node.Val)
+		if node.Next != nil {
+			fmt.Print("->")
+		}
+		node = node.Next
+	}
+	fmt.Println()
 }
--- lists_test.go
+++ lists_test.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 )
 
@@ -29,5 +30,35 @@ func TestAddTwoNumbers(t *testing.T) {
 	sum := AddTwoNumbers2(l1, l2)
 	s, _ := json.Marshal(sum)
 	t.Log(string(s))
+}
 
+var head = &ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val: 4,
+				Next: &ListNode{
+					Val: 5,
+				},
+			},
+		},
+	},
+}
+
+var oneNodeHead = &ListNode{
+	Val: 1,
+}
+
+func TestListPrint(t *testing.T) {
+	ListPrint(head)
+}
+
+func TestReversePrint(t *testing.T) {
+	ListPrint(head)
+	fmt.Println(ReversePrint4(head))
+	ListPrint(oneNodeHead)
+	fmt.Println(ReversePrint4(oneNodeHead))
 }
