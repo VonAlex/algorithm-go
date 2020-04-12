23d067d192f48bce7433e55438a33710eeec42b2
VonAlex <Ahappyhappen@163.com>
2020-04-12 18:18:44 +0800

    :sparkles: solve  reverse string

--- strings.go
+++ strings.go
@@ -224,3 +224,23 @@ func IsPalindrome(s string) bool {
 	}
 	return true
 }
+
+/**
+ * leetcode 题 344 反转字符串
+ * 编写一个函数，其作用是将输入的字符串反转过来。
+ * 说明：不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ *
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ *
+ * https://leetcode-cn.com/problems/reverse-string/
+ */
+func ReverseString(s []byte) {
+	l := 0
+	r := len(s) - 1
+	for l < r {
+		s[l], s[r] = s[r], s[l]
+		l++
+		r--
+	}
+}
