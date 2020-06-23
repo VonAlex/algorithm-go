3b2d9a153856df96e4002808e90aecfd73769c81
VonAlex <Ahappyhappen@163.com>
2020-06-23 22:41:39 +0800

    :sparkles: ac max profit

--- arrays.go
+++ arrays.go
@@ -1012,7 +1012,7 @@ func SortColors2(nums []int) {
 	}
 }
 
-/**
+/*
  * LeetCode T350. 两个数组的交集 II
  * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
  *
@@ -1026,7 +1026,6 @@ func SortColors2(nums []int) {
  * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
  * 我们可以不考虑输出结果的顺序。
  */
-
 // 方法 1：哈希映射
 // 时间复杂度 O(m+n)，空间复杂度 O(n)
 func intersect(nums1 []int, nums2 []int) []int {
@@ -1071,4 +1070,55 @@ func intersect2(nums1 []int, nums2 []int) []int {
 		}
 	}
 	return res
+}
+
+/*
+ * LeetCode T122. 买卖股票的最佳时机 II
+ * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+ *
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）
+ */
+// 方法 1：贪心法
+// 股票买卖策略: 单独交易日/连续上涨交易日/连续下降交易日
+// 连续上涨交易日： 设此上涨交易日股票价格分别为 p1,p2..pn, 则第一天买最后一天卖收益最大, pn-p1
+// 等价于每天都买卖,即 pn-p1 = (p2-p1)+(p3-p2)+...+(pn-pn-1)
+// 从贪心算法思路看，就是逢低就买入，逢高就卖出。贪心算法就是说一个目光短浅的贪心的人，只会考虑下一步的得失，从不考虑长远的利益
+// 时间复杂度：O(n),空间复杂度：O(1)
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	lens := len(prices)
+	for i := 0; i < lens-1; i++ {
+		p := prices[i+1] - prices[i]
+		if p > 0 {
+			maxProfit += p
+		}
+	}
+	return maxProfit
+}
+
+// 方法 2：峰谷法
+// 时间复杂度：O(n),空间复杂度：O(1)
+// 所有的波峰 - 波谷
+func maxProfit2(prices []int) int {
+	maxProfit := 0
+	lens := len(prices)
+	if lens == 0 {
+		return maxProfit
+	}
+	peak := prices[0]
+	valley := prices[0]
+	for i := 0; i < lens-1; {
+		for i < lens-1 && prices[i] >= prices[i+1] {
+			i++
+		}
+		valley = prices[i]
+		for i < lens-1 && prices[i] <= prices[i+1] {
+			i++
+		}
+		peak = prices[i]
+		maxProfit += peak - valley
+	}
+	return maxProfit
 }
