72b5888a10daaa212d22a0c879419e9c0815aa70
VonAlex <Ahappyhappen@163.com>
2020-09-26 09:21:33 +0800

    :sparkles: ac intersection of two arrays

--- arrays.go
+++ arrays.go
@@ -952,7 +952,7 @@ func SubarraySum4(nums []int, k int) int {
 	return res
 }
 
-/**
+/*
  * LeetCode T75. 颜色分类
  * https://leetcode-cn.com/problems/sort-colors/
  *
@@ -1010,4 +1010,39 @@ func SortColors2(nums []int) {
 			curr++
 		}
 	}
+}
+
+/*
+ * LeetCode T349. 两个数组的交集
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays/
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ *
+ * 输入: [2,0,2,1,1,0]
+ * 输出: [0,0,1,1,2,2]
+ *
+ * 说明：
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序
+ */
+// 假设 nums1/2 长度分别为m/n,
+// 空间复杂度为 O(m),m 可以取两者较小值，时间复杂度为 O(m+n)
+func intersection(nums1 []int, nums2 []int) []int {
+	nums1Set := make(map[int]struct{})
+	for _, num := range nums1 {
+		nums1Set[num] = struct{}{}
+	}
+	var res []int
+	for _, num := range nums2 {
+		if _, ok := nums1Set[num]; ok {
+			res = append(res, num)
+			delete(nums1Set, num)
+		}
+	}
+	return res
 }
